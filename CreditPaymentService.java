cdd24af04077aeb76b7bf314c8324f74b01bcfce
Anastasiya Ganieva <ganievrav@rambler.ru>
2021-03-17 18:37:04 +0400

    .kkk

---
+++ CreditPaymentService.java
@@ -0,0 +1,7 @@
+public class CreditPaymentService{
+    public int calculate(int summaCr,int srok, double percenSt) {
+        double r =(double) ((percenSt/100)/12);
+        int platej =(int) (summaCr * ((r*Math.pow((1+r),srok))/(Math.pow((1+r),srok)-1)));
+        return platej;
+}
+}
