8a773fe5b0f3eb0a77ec97339d00dcd9f5c728b2
AnastasiiaGanieva <69317903+AnastasiiaGanieva@users.noreply.github.com>
2021-03-20 20:13:02 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CreditPaymentService a = new CreditPaymentService();
-        System.out.println(a.calculate(1000000,12,9.99));
-        System.out.println(a.calculate(1000000,24,9.99));
-        System.out.println(a.calculate(1000000,36,9.99));
+        System.out.println(a.calculate(1000000, 12, 9.99));
+        System.out.println(a.calculate(1000000, 24, 9.99));
+        System.out.println(a.calculate(1000000, 36, 9.99));
     }
 }
